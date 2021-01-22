1572f089c7b49a18f3a68f7a91ebbe2a9acecf0c
key12pat34 <keyurpatil@gmail.com>
2021-01-21 21:43:22 -0500

    map.js updated

--- Project/Starter Code/js/map.js
+++ Project/Starter Code/js/map.js
@@ -157,3 +157,82 @@ function mapStyle(feature) {
 
 
 
+
+
+
+
+
+
+
+
+// //drawing function 
+// var drawingManager = new google.maps.drawing.DrawingManager();
+//   drawingManager.setMap(map);
+  
+
+
+// // This example requires the Drawing library. Include the libraries=drawing
+// // parameter when you first load the API. For example:
+// // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=drawing">
+// function initMap() {
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+//   const drawingManager = new google.maps.drawing.DrawingManager({
+//     drawingMode: google.maps.drawing.OverlayType.MARKER,
+//     drawingControl: true,
+//     drawingControlOptions: {
+//       position: google.maps.ControlPosition.TOP_CENTER,
+//       drawingModes: [
+//         google.maps.drawing.OverlayType.MARKER,
+//         google.maps.drawing.OverlayType.CIRCLE,
+//         google.maps.drawing.OverlayType.POLYGON,
+//         google.maps.drawing.OverlayType.POLYLINE,
+//         google.maps.drawing.OverlayType.RECTANGLE,
+//       ],
+//     },
+//     markerOptions: {
+//       icon:
+//         "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+//     },
+//     circleOptions: {
+//       fillColor: "#ffff00",
+//       fillOpacity: 1,
+//       strokeWeight: 5,
+//       clickable: false,
+//       editable: true,
+//       zIndex: 1,
+//     },
+//   });
+//   drawingManager.setMap(map);
+// }
+
+// drawingManager.setOptions({
+//   drawingControlOptions: {
+//     position: google.maps.ControlPosition.BOTTOM_LEFT,
+//     drawingModes: ['marker']
+//   }
+// });
+
+// // To hide:
+// drawingManager.setOptions({
+//   drawingControl: false
+// });
+
+// // To show:
+// drawingManager.setOptions({
+//   drawingControl: true
+// });
+
+// // drawingManager.setMap(null);
+
+// google.maps.event.addListener(drawingManager, 'circlecomplete', function(circle) {
+//   var radius = circle.getRadius();
+// });
+
+// google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
+//   if (event.type == 'circle') {
+//     var radius = event.overlay.getRadius();
+//   }
+// });
--- Project/v1.0/js/config.js
+++ Project/v1.0/js/config.js
@@ -1,2 +1,3 @@
 // API key
 const API_KEY = "pk.eyJ1IjoiZGFzc2Nvb3RlciIsImEiOiJja2pwdHZzNHoxMXlmMnhwZWZkaWp6d2xiIn0.zc1HfeVEMzRwCEm_glvbUQ"
+// const GoogleAPI = "AIzaSyBziBa1em_bBCXGyYMJwelg4R-GnyMjgHE"
