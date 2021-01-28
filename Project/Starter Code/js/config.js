cb7cc3e7b6770f0e42708b5f4dbc4408393367a1
Scooters1206 <rpsiii@gmail.com>
2021-01-28 17:44:16 -0500

    Cleaned Flask

--- Project/Starter Code/js/config.js
+++
@@ -1,3 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1IjoiZGFzc2Nvb3RlciIsImEiOiJja2pwdHZzNHoxMXlmMnhwZWZkaWp6d2xiIn0.zc1HfeVEMzRwCEm_glvbUQ"
-// const GoogleAPI = "AIzaSyBziBa1em_bBCXGyYMJwelg4R-GnyMjgHE"
